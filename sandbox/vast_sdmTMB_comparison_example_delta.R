5141709d2cac86ef0f267333c46fd948e67d96f8
Chantel Wetzel <chantelwetzel@noaa.gov>
2022-05-17 17:18:40 -0700

    ex: current delta example code

--- sandbox/vast_sdmTMB_comparison_example_delta.R
+++ sandbox/vast_sdmTMB_comparison_example_delta.R
@@ -1,17 +1,33 @@
 library(VAST)
 library(VASTWestCoast)
+#devtools::load_all("C:/Users/Chantel.Wetzel/Documents/GitHub/VASTWestCoast")
 library(sdmTMB)
+#devtools::load_all("C:/Users/Chantel.Wetzel/Documents/GitHub/sdmTMB")
+library(sp)
 library(here)
+library(dplyr)
+library(ggplot2)
 
+setwd("C:/Users/Chantel.Wetzel/Documents/GitHub/indexwc/sandbox")
 here::i_am("vast_sdmTMB_comparison_example.R")
-species = "dover_example"
-dir.create(here(species))
-setwd(here(species))
+data_dir = here("Catch__NWFSC.Combo_2022-04-09.rdata")
+dir.create(here(paste0("gamma_example/vessel_year_main")))
+dir = here(paste0("gamma_example/vessel_year_main"))
 
 # Load catch data file for dover sole in the "dover_example" folder
 load("Catch__NWFSC.Combo_2022-04-09.rdata")
+# clean and add needed columns to the data
 data <- VASTWestCoast::clean_data(data = Out)
-catchability_data <- data[,"Pass", drop = FALSE] - mean(range(data[,"Pass"]))
+# add catch and cpue in terms of metric tons
+data[,"Catch_mt"] <- data[,"Catch_KG"] / 1000
+data[,"cpue_mt_km2"] <- data[,"Catch_mt"] / data[,"AreaSwept_km2"]
+units(data[, "Catch_mt"]) <- 't'
+# add effort = 1 column for specific VAST / sdmTMB comparisons
+data$effort <- 1
+# add factors and random effects terms
+data[,"pass_scaled"] <- data[,"Pass", drop = FALSE] - mean(range(data[,"Pass"]))
+data[,"vessel_scaled"] <- as.numeric(data[, "Vessel"], as.is = FALSE) - 1
+
 
 # Set up the Field and Rho Configuration for VAST
 FieldConfig <- matrix(c("IID", "IID", "IID", "IID", "IID", "IID"),
@@ -39,146 +55,103 @@ settings <- FishStatsUtils::make_settings(
 
 # Create the mesh for VAST
 survey <- VASTWestCoast::convert_survey4vast(survey = "WCGBTS")
-mesh <- VASTWestCoast::VAST_mesh(data = data,
+vast_mesh <- VASTWestCoast::VAST_mesh(data = data,
   survey = survey,
-  numknots = as.numeric(settings['n_x']))
+  numknots = as.numeric(settings['n_x']),
+  savedir = dir)
 
 # Set the data to the updated data frame create within the VAST_mesh fxn
-subdata <- mesh$mesh$data.inner
+# this adds the X and Y coordinates
+subdata <- vast_mesh$mesh$data.inner
 
 # Run VAST with simplified model structure
-
 fit <- fit_model(
   settings = settings,
   Lat_i = subdata[, "Lat"],
   Lon_i = subdata[, "Lon"],
   t_i = subdata[, "Year"],
-  b_i = subdata[, "Catch_KG"],
-  a_i = subdata[, "AreaSwept_km2"],
-  #v_i = as.numeric(subdata[, "Vessel"], as.is = FALSE) - 1,
-  working_dir = getwd(),
+  b_i = subdata[,'Catch_mt'], #subdata[,'cpue_kg_km2'], 
+  a_i = subdata[, "AreaSwept_km2"], #subdata[,"effort"]
+  v_i = subdata[, "vessel_scaled"],
   #Q1_formula = ~ Pass,
-  #Q2_formulat = ~ Pass,
-  catchability_data = catchability_data,
-  extrapolation_args = c(
-    settings['zone'],
-    settings['Region'],
-    settings['strata.limits'],
-    suveyname = survey,
-    input_grid = list(mesh[['inputgrid']]))
+  #Q2_formula = ~ Pass,
+  working_dir = dir,
+  #catchability_data = subdata$pass_scaled,
+  #extrapolation_args = c(
+  #  settings['zone'],
+  #  settings['Region'],
+  #  settings['strata.limits'],
+  #  suveyname = survey,
+  #input_grid = list(mesh[['inputgrid']]))
+  input_grid = vast_mesh[['inputgrid']]
 )
 
 # Calculate the index of abundance from VAST
 index_vast <- suppressWarnings(FishStatsUtils::plot_biomass_index(
   fit = fit,
-  DirName = file.path(getwd(), .Platform$file.sep),
+  DirName = file.path(dir, .Platform$file.sep),
   TmbData = fit$data_list,
   Sdreport = fit$parameter_estimates$SD,
-  use_biascorr = TRUE,
-  Year_Set = fit$year_labels,
-  Years2Include = fit$years_to_plot,
+  use_biascorr = FALSE,
+  year_labels = as.numeric(fit$year_labels),
+  years_to_plot = fit$years_to_plot,
   strata_names = fit$settings$strata.limits$STRATA
 ))
 
-# Create the Table_for_SS3 csv from the Index.csv with needed corrections
-# for our indices
-fileindex <- file.path(getwd(), "Table_for_SS3.csv")
-indexdata <- data.frame(
-  Year = fit$year_labels[index_vast[["Table"]][, "Time"]],
-  Unit = "mt", #index_vast[["Table"]][, "Units"],
-  Fleet = index_vast[["Table"]][, "Stratum"],
-  # Go from kg to mt to keep backwards compatibility with VASTWestCoast
-  Estimate_metric_tons = index_vast[["Table"]][, "Estimate"] / 1000,
-  SD_log = index_vast[["Table"]][, "Std. Error for ln(Estimate)"],
-  SD_mt = index_vast[["Table"]][, "Std. Error for Estimate"] / 1000
-)
-utils::write.csv(x = indexdata, file = fileindex, row.names = FALSE)
+save(fit, index_vast, vast_mesh, settings,
+  file = file.path(dir, "vast_save.RData"))
 
-save(fit, index_vast, mesh, settings,
-  file = file.path(getwd(), "vast_save.RData"))
+save(data, subdata,
+  file = file.path(dir, "data.RData"))
 
 ########################################################################
 # sdmTMB
 ########################################################################
 
 # create mesh for sdmTMB from mesh used by VAST
-mesh <- sdmTMB::make_mesh(
+mesh_sdmTMB <- sdmTMB::make_mesh(
   data = subdata,
   xy_cols = c("X", "Y"),
   mesh = fit$spatial_list$MeshList$isotropic_mesh
 )
-plot(mesh)
+#plot(mesh_sdmTMB)
 
 # Run simplified model structure for sdmTMB
+subdata$catch_mt <- drop_units(subdata$Catch_mt)
+
+tictoc::tic()
 fit_sdmTMB <- sdmTMB::sdmTMB(
-  Catch_KG ~ 0 + as.factor(Year), # + as.factor(Pass) + (1 | Vessel),
+  catch_mt ~ 0 + as.factor(Year) +  as.factor(1 | vessel_scaled),
   time = "Year",
   offset = log(subdata$AreaSwept_km2),
   data = subdata,
-  mesh = mesh,
+  mesh = mesh_sdmTMB,
   family = delta_gamma(),
   spatial = "on",
   spatiotemporal = "iid",
   control = sdmTMBcontrol(newton_loops = 1), # match VAST default settings
-  silent = FALSE
+  silent = TRUE
 )
+tictoc::toc()
 
 print(fit_sdmTMB)
-
-sanity(fit_sdmTMB) # experimental... function name may change
-
-# Create predictions
-pred <- predict(
-  fit_sdmTMB,
-  newdata = subdata, # SA: fix this!
-  return_tmb_object = TRUE
-)
-
-# Create index with bias correction
-index_sdmTMB <- sdmTMB::get_index(pred, bias_correct = FALSE)
-
-save(mesh, fit_sdmTMB, pred, index_sdmTMB,
-  file = file.path(getwd(), "sdmTMB_save.RData")
-)
+print_iid_re(fit_dg, model = 1)
+print_iid_re(fit_dg, model = 2)
+# sanity(fit_sdmTMB) # experimental... function name may change
 
 ###########################################################################
 # Compare parameter estimates between VAST and sdmTMB
 ###########################################################################
+source(here("plot_betas.R"))
 
-plot_betas <- function(vast_model, sdmTMB_model, years, vast_par = "beta1_ft", sdmTMB_pars = 1) {
-  s <- vast_model$parameter_estimates$SD
-  vast_est1 <- as.list(s, "Estimate", report = FALSE)
-  vast_est2 <- as.list(s, "Estimate", report = TRUE)
-  vast_sd1 <- as.list(s, "Std. Error", report = FALSE)
-  vast_sd2 <- as.list(s, "Std. Error", report = TRUE)
-  sdmTMB_est <- as.list(sdmTMB_model$sd_report, "Estimate", report = FALSE)
-  sdmTMB_sd <- as.list(sdmTMB_model$sd_report, "Std. Error", report = FALSE)
-  b_year_vast <- vast_est1[[vast_par]][!is.na(vast_sd1[[vast_par]])]
-  b_year_vast_se <- vast_sd1[[vast_par]][!is.na(vast_sd1[[vast_par]])]
-  lwr_vast <- b_year_vast - 2 * b_year_vast_se
-  upr_vast <- b_year_vast + 2 * b_year_vast_se
-  plot(years, b_year_vast, ylim = range(c(lwr_vast, upr_vast)))
-  segments(years, lwr_vast, years, upr_vast)
-  years <- years + 0.05
-  if (sdmTMB_pars == 1) {
-    points(years, sdmTMB_est$b_j)
-    segments(years, sdmTMB_est$b_j - 2 * sdmTMB_sd$b_j,
-      years, sdmTMB_est$b_j + 2 * sdmTMB_sd$b_j,
-      col = "red")
-  } else {
-    points(years, sdmTMB_est$b_j2)
-    segments(years, sdmTMB_est$b_j2 - 2 * sdmTMB_sd$b_j2,
-      years, sdmTMB_est$b_j2 + 2 * sdmTMB_sd$b_j2,
-      col = "red")
-  }
-  legend("topright", legend = c("VAST", "sdmTMB"),
-    col = c("black", "red"), bty = "n", lty = c(1, 1))
-}
-
+out_file = file.path(dir, "parameter_comparison.png")
+grDevices::png(filename = out_file,
+  width = 10, height = 7, units = "in", res = 300, pointsize = 12)
 par(mfrow = c(2, 1), cex = 0.8, mar = c(1.5, 1, 1, 1), oma = c(2, 3, 1, 1))
 years <- sort(unique(subdata$Year))
-plot_betas(fit, fit_sdmTMB, "beta1_ft", sdmTMB_pars = 1, years = years)
-plot_betas(fit, fit_sdmTMB, "beta2_ft", sdmTMB_pars = 2, years = years)
+plot_betas(vast_model = fit, sdmTMB_model = fit_sdmTMB, "beta1_ft", sdmTMB_pars = 1, years = years)
+plot_betas(vast_model = fit, sdmTMB_model = fit_sdmTMB, "beta2_ft", sdmTMB_pars = 2, years = years)
+dev.off()
 
 # fit$parameter_estimates$SD
 s1 <- fit$ParHat$beta1_ft[fit$ParHat$beta1_ft != 0]
@@ -186,12 +159,87 @@ s2 <- fit$ParHat$beta2_ft[fit$ParHat$beta2_ft != 0]
 
 b1 <- tidy(fit_sdmTMB, model = 1)
 b2 <- tidy(fit_sdmTMB, model = 2)
+#b1 <- b1[b1$term != "as.factor(Pass)2", ]
+#b2 <- b2[b2$term != "as.factor(Pass)2", ]
 # b_only_sdmTMB <- b_sdmTMB$estimate[b_sdmTMB$term != "as.factor(Pass)2"]
 
+out_file = file.path(dir, "s1_b1.png")
+grDevices::png(filename = out_file,
+  width = 10, height = 7, units = "in", res = 300, pointsize = 12)
+par(mfrow = c(2, 1), cex = 0.8, mar = c(1.5, 1, 1, 1), oma = c(2, 3, 1, 1))
 plot(s1, b1$estimate);abline(0, 1)
 plot(s2, b2$estimate);abline(0, 1)
+dev.off()
+
 s1 - b1$estimate
 s2 - b2$estimate
+
+###########################################################################
+# Create the same grid used by vast for sdmTMB
+###########################################################################
+
+survey <- VASTWestCoast::convert_survey4vast(survey = "WCGBTS")
+grid <- VASTWestCoast::get_inputgrid(survey = survey)
+vast_grid <- grid
+vast_grid <- vast_grid[vast_grid$Area_km2 > 0, ]
+coordinates(vast_grid) <- c("Lon", "Lat")
+proj4string(vast_grid) <- CRS("+proj=longlat +datum=WGS84")
+newproj <- paste("+proj=utm +zone=10 ellps=WGS84 +datum=WGS84")
+grid_trans <- spTransform(vast_grid, CRS(newproj))
+grid_trans <- as.data.frame(grid_trans)
+vast_grid$X <- grid_trans[,"Lon"] / 1000 # convert to km
+vast_grid$Y <- grid_trans[,"Lat"] / 1000 # convert to km
+
+# Random setting to test - but definitely not correct
+vast_grid$vessel_scaled <- factor(mean(subdata$vessel_scaled))
+vast_grid$pass_scaled <- factor(mean(subdata$pass_scaled))
+
+# Create a grid for each year with all factors included
+year_grid <- NULL
+for(y in c(2003:2019, 2021)){
+  vast_grid$Year <- y
+  year_grid <- rbind(year_grid, data.frame(vast_grid[, c("X", "Y", "Year", "Area_km2", "vessel_scaled", "pass_scaled")]))
+}
+
+###########################################################################
+# Create predictions and index using grid
+###########################################################################
+pred <- predict(
+  fit_sdmTMB,
+  newdata = year_grid, # the grid in question
+  return_tmb_object = TRUE 
+)
+
+# Create index with bias correction
+tictoc::tic()
+index_sdmTMB <- sdmTMB::get_index(pred, 
+  bias_correct = FALSE,
+  area = year_grid$Area_km2)
+tictoc::toc()
+
+
+save(mesh_sdmTMB, fit_sdmTMB, year_grid, pred, index_sdmTMB,
+  file = file.path(dir, "sdmTMB_save_error_in_predict.RData")
+)
+
+###########################################################################
+# Bind and plot indices
+###########################################################################
+
+source(here("bind_indices.R"))
+source(here("plot_indices.R"))
+
+both_i <- bind_indices(dir = dir, sdmtmb_index = index_sdmTMB)
+save(both_i, file = file.path(dir, "both_indices.Rdata"))
+plot_indices(data = both_i, 
+  main_name = "Dover sole - Year (metric tons)", 
+  save_loc = dir, 
+  ymax = NULL) 
+
+
+###########################################################################
+# More Parameter Comparisons
+###########################################################################
 
 s <- fit$parameter_estimates$SD
 vast_est1 <- as.list(s, "Estimate", report = FALSE)
@@ -227,3 +275,4 @@ sdmtmb_est2$estimate[sdmtmb_est2$term == "phi"]
 
 # VAST: shape = 1/CV^2
 1/exp(vast_est1$logSigmaM[1,1])^2
+
