eb5aacb386eaeb83be04996ab16ab8bc67d7033e
Chantel Wetzel <chantelwetzel@noaa.gov>
2022-05-19 16:53:46 -0700

    add: generalized comparison run for multiple species
    Former-commit-id: 0037b275186f7ac6e1825edfb1871ccbcbe8c46d [formerly 2c9e97393ebe04fa5458716edbbd4ce4b1940fd8 [formerly a476e7d162129df4f6e4c894471853fe4f45dd15] [formerly 1a7d3e71e5ec983199b07821add0bf2c603210da [formerly a7eb10db886b98b130753bfbcc3ebe77fd7e51f0]] [formerly 2a20c25c2fa1f93cbb8194a07af990724338dc9c [formerly c44bb64685cf0ecad821d324c8950f85dfaff31d] [formerly 0037b275186f7ac6e1825edfb1871ccbcbe8c46d [formerly 07777cbaa836f44aab102c27c82dfb196fa3031b]]]]
    Former-commit-id: 76d12645ccc2d03e87e025389dd1791df4718552
    Former-commit-id: d936517c451283360489600c042a467d99b37472 [formerly 63d23a0adbb392e8935e8d22c0a14052c2926176] [formerly 8a18088ebbfb15fbabfa343f33760cb90fef97ec [formerly 8a463979345043153417f0bddbb598a4584be03c]]
    Former-commit-id: d9e17b59dd8649023a942c772c3d09d172d8bb75 [formerly e3998874bbbe6a5fb871a9bd1ea73378210928df]
    Former-commit-id: fe249b11a62679fc24118fe3b527bf656aea5512
    Former-commit-id: b9a5311b7f0e80d3c41ab1ea671cb03410c6e643 [formerly bca527d62a0fe5c9d66d6d1d56ef3334cdf05a00] [formerly d635be483f7ad49729a1ff3b48882d45a62a72ca [formerly 5d1ee43f4457b85e435511021e3b9988de033a5d]]
    Former-commit-id: d22a3343ba0870045fe931d18b239617ef3df931 [formerly 7502aa2b3105975042e61a3dad899d39cbc64edb]
    Former-commit-id: d21f783497a6c35629fbbd3093745fe14b160ede

---
+++ sandbox/run_index_comparison.R
@@ -0,0 +1,254 @@
+# Load libraries
+library(VAST)
+library(VASTWestCoast)
+library(sdmTMB)
+library(here)
+library(dplyr)
+library(ggplot2)
+TMB::openmp(n = 4L) # because VAST may be picking this up now with TMBad?
+options(sdmTMB.cores = 4L)
+
+setwd("C:/Users/Chantel.Wetzel/Documents/GitHub/indexwc/sandbox")
+source("do_vast_settings.R")
+source("format_data.R")
+source("utils.R")
+source("plot_indices.R")
+
+species = c("arrowtooth_flounder",
+			"big_skate",
+			"chilipper",
+			"darkblotched_rockfish",
+			"aurora_rockfish",
+			"bocaccio",
+			"sablefish", 
+			"rougheye_rockfish",
+			"canary_rockfish",
+			"pacific_ocean_perch",
+			"shortspine_thornyhead",
+			"petrale_sole",
+			"pacific_spiny_dogfish",
+			"widow_rockfish",
+			"yelloweye_rockfish")
+
+anis = FALSE
+bias_correct = FALSE
+
+for (sp in species) {
+
+# May need to add a remove call here to clear workspace of previous estimates
+
+dir <- file.path(getwd(), "comparisons", sp)
+if (anis){
+	dir <- file.path(getwd(), "anisotropy", sp) }
+if (bias_correct){
+	dir <- file.path(getwd(), "bias_correct", sp)  }
+if (bias_correct & anis){
+	dir <- file.path(getwd(), "anisotropy_bias_correct", sp) }
+
+#dir <- file.path(getwd(), "spatiotemporal_off", sp)
+dir.create(dir, showWarnings = FALSE)
+
+# Load catch data file
+load(file.path(getwd(), "data", paste0(sp, "_catch_data.rdata")))
+data <- format_data(Out = Out)
+
+# Set up the Field and Rho Configuration for VAST
+settings <- do_vast_settings(knots = 150, obs_model = c(2, 0), 
+	anis = anis, bias = bias_correct)
+
+# Create the mesh for VAST
+survey <- VASTWestCoast::convert_survey4vast(survey = "WCGBTS")
+vast_mesh <- VASTWestCoast::VAST_mesh(data = data,
+  survey = survey,
+  numknots = as.numeric(settings['n_x']),
+  savedir = dir)
+
+# Set the data to the updated data frame create within the VAST_mesh fxn
+# this adds the X and Y coordinates
+subdata <- vast_mesh$mesh$data.inner
+
+fit <- fit_model(
+  settings = settings,
+  Lat_i = subdata[, "Lat"],
+  Lon_i = subdata[, "Lon"],
+  t_i = subdata[, "Year"],
+  b_i = subdata[,'Catch_mt'], 
+  a_i = subdata[, "AreaSwept_km2"],
+  v_i = subdata[, "vessel_scaled"],
+  Q1_formula = ~ pass_scaled,
+  Q2_formula = ~ pass_scaled,
+  working_dir = dir,
+  catchability_data = subdata,
+  input_grid = vast_mesh[['inputgrid']]
+)
+
+index_vast <- extract_vast_index(x = fit)
+
+########################################################################
+# Save VAST and data objects
+########################################################################
+
+save(fit, index_vast, vast_mesh, settings,
+  file = file.path(dir, "vast_save.RData"))
+
+save(data, subdata,
+  file = file.path(dir, "data.RData"))
+
+########################################################################
+# Run sdmTMB
+########################################################################
+
+if (anis){
+	mesh <- fit$spatial_list$MeshList$anisotropic_mesh
+} else {
+	mesh <- fit$spatial_list$MeshList$isotropic_mesh
+}
+
+# create mesh for sdmTMB from mesh used by VAST
+mesh_sdmTMB <- sdmTMB::make_mesh(
+  data = subdata,
+  xy_cols = c("X", "Y"),
+  mesh = mesh
+)
+
+# Modify the suddata data frame to run with sdmTMB
+subdata$catch_mt <- drop_units(subdata$Catch_mt)
+subdata$vessel_scaled <- as.factor(subdata$vessel_scaled)
+
+fit_sdmTMB <- sdmTMB(
+  catch_mt ~ 0 + as.factor(Year) + pass_scaled + (1 | vessel_scaled),
+  time = "Year",
+  offset = log(subdata$AreaSwept_km2),
+  data = subdata,
+  mesh = mesh_sdmTMB,
+  family = delta_gamma(),
+  spatial = "on",
+  spatiotemporal = "off",
+  anisotropy = anis, 
+  silent = FALSE,
+  control = sdmTMBcontrol(newton_loops = 1L)
+)
+
+# print(fit_sdmTMB)
+# tidy(fit_sdmTMB, model = 1)
+# tidy(fit_sdmTMB, model = 2)
+# sanity(fit_sdmTMB) # experimental... function name may change
+
+###########################################################################
+# Compare parameter estimates between VAST and sdmTMB
+###########################################################################
+
+out_file = file.path(dir, "parameter_comparison.png")
+grDevices::png(filename = out_file,
+  width = 10, height = 7, units = "in", res = 300, pointsize = 12)
+par(mfrow = c(2, 1), cex = 0.8, mar = c(4, 4, 2, 2), oma = c(2, 3, 1, 1))
+plot_betas_delta(vast_model = fit, sdmTMB_model = fit_sdmTMB, "beta1_ft", sdmTMB_pars = 1)
+plot_betas_delta(vast_model = fit, sdmTMB_model = fit_sdmTMB, "beta2_ft", sdmTMB_pars = 2)
+dev.off()
+
+# fit$parameter_estimates$SD
+s1 <- fit$ParHat$beta1_ft[fit$ParHat$beta1_ft != 0]
+s2 <- fit$ParHat$beta2_ft[fit$ParHat$beta2_ft != 0]
+
+b1 <- tidy(fit_sdmTMB, model = 1)
+b2 <- tidy(fit_sdmTMB, model = 2)
+
+yr_i <- grep("year", b1$term, ignore.case = TRUE) # find year coefs
+
+out_file = file.path(dir, "s1_b1.png")
+grDevices::png(filename = out_file,
+  width = 10, height = 7, units = "in", res = 300, pointsize = 12)
+par(mfrow = c(2, 1), cex = 0.8, mar = c(1.5, 1, 1, 1), oma = c(2, 3, 1, 1))
+plot(s1, b1$estimate[yr_i], xlab = "VAST s1", ylab = "sdmTMB b1");abline(0, 1)
+plot(s2, b2$estimate[yr_i], xlab = "VAST s2", ylab = "sdmTMB b2");abline(0, 1)
+dev.off()
+
+###########################################################################
+# Create the same grid used by vast for sdmTMB
+###########################################################################
+
+survey <- VASTWestCoast::convert_survey4vast(survey = "WCGBTS")
+grid <- VASTWestCoast::get_inputgrid(survey = survey)
+vast_grid <- grid
+vast_grid <- vast_grid[vast_grid$Area_km2 > 0, ]
+vast_grid <- sdmTMB::add_utm_columns(vast_grid, c("Lon", "Lat"), utm_crs = 32610) # UTM 10
+vast_grid$pass_scaled <- 0
+vast_grid$vessel_scaled <- subdata$vessel_scaled[1]
+vast_grid <- dplyr::select(vast_grid, X, Y, Area_km2, pass_scaled, vessel_scaled)
+
+year_grid <- purrr::map_dfr(c(2003:2019, 2021), function(yr) {
+  vast_grid$Year <- yr
+  vast_grid
+})
+
+###########################################################################
+# Create predictions and index using grid
+###########################################################################
+
+fit_sdmTMB$tmb_data$simulate_t <- rep(0L, length(unique(subdata$Year)))
+
+pred <- predict(
+  fit_sdmTMB,
+  re_form_iid = NA,
+  newdata = year_grid,
+  return_tmb_object = TRUE
+)
+
+out_file = file.path(dir, "prediction_maps.png")
+grDevices::png(filename = out_file,
+  width = 10, height = 7, units = "in", res = 300, pointsize = 12)
+par(mfrow = c(1, 2), cex = 0.8, mar = c(1.5, 1, 1, 1), oma = c(2, 3, 1, 1))
+filter(pred$data, Year == 2021) %>%
+  ggplot(aes(X, Y, colour = plogis(est1))) + geom_point() +
+  scale_colour_viridis_c()
+
+filter(pred$data, Year == 2021) %>%
+  ggplot(aes(X, Y, colour = exp(est2))) + geom_point() +
+  scale_colour_viridis_c(trans = "log10")
+dev.off()
+
+p <- pred$data
+ind <- group_by(p, Year) %>%
+  summarise(total = sum(plogis(est1) * exp(est2) * Area_km2))
+
+# Create index:
+index_sdmTMB <- sdmTMB::get_index(
+  pred,
+  bias_correct = bias_correct,
+  area = year_grid$Area_km2
+)
+
+save(mesh_sdmTMB, fit_sdmTMB, year_grid, pred, index_sdmTMB,
+  file = file.path(dir, "sdmTMB_save.RData")
+)
+
+###########################################################################
+# Bind and plot indices
+###########################################################################
+
+index_sdmTMB$index <- "sdmTMB"
+index_sdmTMB <- rename(index_sdmTMB, year = Year)
+both <- bind_rows(index_sdmTMB, index_vast)
+save(both, file = file.path(dir, "both_indices.Rdata"))
+
+out_file = file.path(dir, "indices_ggplot.png")
+grDevices::png(filename = out_file,
+  width = 10, height = 7, units = "in", res = 300, pointsize = 12)
+g <- ggplot(both, aes(x = year, y = est, ymin = lwr, ymax = upr, colour = index, fill = index)) +
+  geom_ribbon(alpha = 0.1, position = position_dodge(width = 0.00), lty = 3) +
+  geom_line(alpha = 0.8, position = position_dodge(width = 0.00), lwd = 1) +
+  coord_cartesian(expand = FALSE) +
+  theme_bw() +
+  scale_colour_brewer(palette = "Set1") +
+  scale_fill_brewer(palette = "Set1") +
+  ylab("Relative biomass estimate") + xlab("Year") + labs(title = sp) +
+  labs(colour = "Package", fill = "Package")
+print(g)
+dev.off()
+
+plot_indices(data = both,
+  main_name = sp,
+  save_loc = dir,
+  ymax = NULL)
+
+} # end species loop
