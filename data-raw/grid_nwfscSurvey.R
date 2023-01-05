dd8e63b96f3d5333f58c77df10129fd18dd7abd3
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2023-01-05 00:21:10 -0800

    Created run and modified run_sdmtmb
    Function is half way done, it will estimate an index.
    Need to fix the plots and stratifications still.
    Will implement Chantel's diagnostics.

---
+++ data-raw/grid_nwfscSurvey.R
@@ -0,0 +1,40 @@
+path <- fs::path(tempdir(), "california_current_grid.rda")
+url <- "https://github.com/James-Thorson-NOAA/FishStatsUtils/raw/main/data/california_current_grid.rda"
+
+download.file(
+  url = url,
+  destfile = path
+)
+load(path)
+unlink(path)
+
+california_current_grid <- california_current_grid %>%
+  dplyr::rowwise() %>%
+  dplyr::mutate(
+    pass_scaled = 0,
+    vessel_year = 0,
+    propInTriennial = ifelse(Lat < 34.5, 0, propInTriennial),
+    depth = Depth_km * -1000,
+    dplyr::across(
+      .cols = dplyr::matches("propIn[WTS]"),
+      .fns = ~ ifelse(Ngdc_m > -35, 0, .)
+    ),
+    dplyr::across(
+      .cols = dplyr::matches("propIn[WTS]"),
+      .fns = ~ 4 * min(.),
+      .names = "{gsub('propIn', 'area_km2_', {col})}"
+    )
+  )
+
+  california_current_grid <- sdmTMB::add_utm_columns(
+    california_current_grid,
+    utm_crs = 32610,
+    ll_names = c("Lon", "Lat"),
+    utm_names = c("x", "y")
+  ) %>%
+  dplyr::rename(
+    longitude = "Lon",
+    latitude = "Lat"
+  )
+
+usethis::use_data(california_current_grid, overwrite = TRUE)
