5be18ea446b671bc38ea7162aa5126dcd1931977
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2023-01-18 07:22:03 -0800

    fix(plot_qq): Uses correct name of data
    * object was referenced as fit rather than data which is the passed
      argument
    * use [[""]] rather than dollar sign to avoid partial matching
    * fix spacing
    * uses on.exit() immediately after opening a device in case the figure
      causes an error and the function is prematurely aborted
    
    
    Former-commit-id: 7eabd032ea3bfba9c671a143d2eab3d291ca54ad [formerly 07ca6fe95b004c3b344af1ec333dd2e78f850005] [formerly bd6e03383b3cc844277edcbdefbde02067958ed3 [formerly 9abd674aeb0157bd60e69b2be6ab2e6a5ef29a40 [formerly 6d3a27010e4bdc1b558c657d91c94fed281f5fe9] [formerly 8d12bb4a7ba5c85db3b301729b9c9ad31d842b24 [formerly 8d7bb411485fe0cb6f24aaf5dc68c407a0c1a846]] [formerly 49a3279b170dd833f254b2aafc5b622952c0f796 [formerly dee7f117962f934ab34ca89c9e8e576060cf5ae9] [formerly 5caf37d4bb88fc27425c78fb4ee61c6f2200476c [formerly eae9f9b645d60ff36d9fdb9fec53179205fbddf5]]]]]
    Former-commit-id: f728f52b45024207992dc5814b583a79a0858ad6 [formerly cec799371fa44add34d70f4de9261f4cd286b849]
    Former-commit-id: 36c7af185e18e51053fa46fea68df346bf746d02
    Former-commit-id: eade6266c9259cf146c6db7d756f4397e1359a6e [formerly 16c276ca761c64a74f64f152e0c2bb0d500dbe3b] [formerly 111ed64bd78d947b253be401029cea39e60811bb [formerly e82ed8e216f87d28e95ed992b187c3163441488f]]
    Former-commit-id: 67eda90d959d2ab7642726e0c60d568f8f91774a [formerly 9203f5b381745d11e06b9c0e342b3b264d7a9bbe]
    Former-commit-id: 06c233cd54fabed1e7b83281c7d6bbe131494c30
    Former-commit-id: 6d994cdd029f734b38878fe90d1611f8389cd134 [formerly 9b3d058e3aa3682315e4a57fe42757e9b4e6450c] [formerly c6738465bd3d8f257f1ba0c4bff89eaef6da020d [formerly 061c74c958dc2c79ae3dbce2f744ace76c745f47]]
    Former-commit-id: 090269eacff370b00d5ae07ff5e9fb4734e3faba [formerly ea271f214d58fa017001ec2fb690847900ed97d1]
    Former-commit-id: e879188da23efba4bd13ac019552baa06df3bad7

--- R/plot_qq.R
+++ R/plot_qq.R
@@ -1,29 +1,23 @@
-#' Plot the QQ and 1:1 line
+#' Plot the quantile-quantile for `fit` with a 1:1 line
 #'
-#' @param data List created by the sdmTMB:: fit function
-#' @template dir 
+#' @param data A list created by [sdmTMB::sdmTMB()].
+#' @template dir
 #'
-#' 
-#'
-#' @author Chantel Wetzel
+#' @author Chantel R. Wetzel
 #' @export
-#'
-#' 
-#'	
-plot_qq <- function(data, dir){
-
-    resids <- data$data$residuals
+plot_qq <- function(data, dir) {
+  residuals <- data[["data"]][["residuals"]]
 
 	grDevices::png(
 		filename = file.path(dir, "qq.png"),
-      	width = 7, 
-      	height = 7, 
-      	units = "in", 
-      	res = 300, 
-      	pointsize = 12
-    )
-    stats::qqnorm(resids) 
-    stats::qqline(resids)
-    dev.off()
+    width = 7, 
+    height = 7, 
+    units = "in", 
+    res = 300, 
+    pointsize = 12
+  )
+  on.exit(dev.off)
+  stats::qqnorm(residuals) 
+  stats::qqline(residuals)
 
 }
