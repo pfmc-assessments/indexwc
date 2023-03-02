86c274b1b31f371b9341b55ca05411903f5dbfb3
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2023-03-02 13:55:18 -0800

    Removes VASTWestCoast dependency

---
+++ R/filter_grid.R
@@ -0,0 +1,12 @@
+#' Filter a grid created with [lookup_grid()]
+#'
+#' @param grid A grid returned from [lookup_grid()].
+#' @param boundary_north,boundary_south Real numbers providing the northern and
+#'   southern latitudinal boundaries for your desired grid.
+#' @return A data frame with the same structure as `grid`.
+#' @author Kelli F. Johnson
+filter_grid <- function(grid, boundary_north, boundary_south) {
+  sub_grid <- grid %>% 
+    dplyr::filter(latitude < boundary_north) %>%
+    dplyr::filter(latitude >= boundary_south)  
+}
---
+++ data-raw/internal.R
@@ -0,0 +1,10 @@
+southern_BC <- 49.0
+southern_WA <- 46.0
+southern_OR <- 42.0
+southern_CA <- 32.0
+
+usethis::use_data(
+  southern_BC, southern_WA, southern_OR, southern_CA,
+  internal = TRUE,
+  overwrite = TRUE
+)
