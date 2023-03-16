3ef58b5fad76448812ec50fcb5c6f72c42e47733
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2023-03-16 14:20:29 -0700

    refactor(plot_map): spacing
    Former-commit-id: 5756828819a895776c88d6fa73dc808423a849f2 [formerly 60c8fe9948c2c9db4d4a68bb30132633126b9311] [formerly 09a88ab37f29d9a8f275e247089c3c120856fc1d [formerly 17ec816bf1cefb6b2845e2e9bc351dc06f85ffef [formerly 959d780ea617498426c9b8938c908d10cea13591] [formerly 71bd640f451fa2cf830b366b0061539ec4985a6d [formerly 22d4120a7d30d8bbbe7ff1d6803b0c85db8e798e]] [formerly 2455d080189835bbea92c2b198eaff227ac93786 [formerly c1bb916c125320a07c6de3cdcc50c4607dbf7953] [formerly 98aae1db570bc915eb8cc6a9046ad84c2bbd577e [formerly 1c19619838251e7ad59c468ad9bd2b2556140302]]]]]
    Former-commit-id: 39799c1e1cade9ea0b9505c7befd22a428897662 [formerly abff2557a8ff4501e4d1f838f80a13b21de966e7]
    Former-commit-id: 952bc59f2930309de467ab08f2b62c31998fbbcd
    Former-commit-id: 8fed2e1b7fec76855ffde2db30896c3c7d40c33d [formerly 73b49736031643f170305e93e84a6879bd6d0a9e] [formerly faf92b0cb39d2bb75342f85c37e81bcbd9ca52b2 [formerly 94d0a847b4c5a020e58fc3239672f4fb395a09e1]]
    Former-commit-id: fbd6b5e9e9b094cbafa85da5a04e06af3752b22d [formerly d0103e1a3ea233482d93451d168d26ffbf818183]
    Former-commit-id: 8102dedd3f627ce59381f563d5b941328a4b3f9b
    Former-commit-id: e40fdee5a42c933b9d8f05ff079014a039c9a865 [formerly f3511d328168d7a1db2ce08d8f80b1966dc8d080] [formerly ebdcf48b27e427818b746ecf0660cacf96779292 [formerly c9e46a2c39d6f845e83abcc996845bbafc1e003d]]
    Former-commit-id: 68dd132d65fe27540175d92a35811a5a6d848a2a [formerly 200d76506de240ea501cbd4e1e13e1ce15d987c9]
    Former-commit-id: 0921aeb9461042778291413c9544f295a31d210d

--- R/plot_map.R
+++ R/plot_map.R
@@ -1,28 +1,25 @@
-#' Map plotting basic settings applied to all
-#' mapping diagnostic figures using ggplot2
+#' Map using [ggplot2::ggplot()]
 #'
 #'
-#' @param data Predictions data frame created by 
-#' the sdmTMB::predict function and the prediction
-#' grid.
-#' @param column The column name to plot within
-#' the plotting function (e.g., est_non_rf2, 
-#' omega_s2, epsilon_st2).
+#' @param data Predictions data frame created by [predict()] and the prediction
+#'   grid.
+#' @param column The column name to plot within the plotting function (e.g.,
+#'   est_non_rf2, omega_s2, epsilon_st2).
 #' 
 #' @import ggplot2
 #'
-#' @author Chantel Wetzel
+#' @author Chantel R. Wetzel
 #' @export
-#' 
+#'
 plot_map <- function(data, column) {
 
-	lon_range <- c(min(data$lon), max(data$lon))
-  	lat_range <- c(min(data$lat), max(data$lat))
+  lon_range <- c(min(data$lon), max(data$lon))
+  lat_range <- c(min(data$lat), max(data$lat))
 
-	ggplot2::ggplot(data, aes(lon, lat, fill = {{ column }})) +
-		geom_raster() +
-		coord_fixed() +
-		nwfscSurvey::draw_theme() +
-      	nwfscSurvey::draw_land() +
-      	nwfscSurvey::draw_USEEZ(lon_range, lat_range) 
+  ggplot2::ggplot(data, aes(lon, lat, fill = {{ column }})) +
+    geom_raster() +
+    coord_fixed() +
+    nwfscSurvey::draw_theme() +
+    nwfscSurvey::draw_land() +
+    nwfscSurvey::draw_USEEZ(lon_range, lat_range)
 }
