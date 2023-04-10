777f5df56178cce8dab169216aef63e158932820
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2023-04-09 21:27:31 -0700

    Adds utm zones to internal data

--- data-raw/internal.R
+++ data-raw/internal.R
@@ -3,8 +3,13 @@ southern_WA <- 46.0
 southern_OR <- 42.0
 southern_CA <- 32.0
 
+
+utm_zone_09 <- 3156
+utm_zone_10 <- 32610
+
 usethis::use_data(
   southern_BC, southern_WA, southern_OR, southern_CA,
+  utm_zone_09, utm_zone_10,
   internal = TRUE,
   overwrite = TRUE
 )
