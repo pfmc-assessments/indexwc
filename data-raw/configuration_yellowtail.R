32f33f63a09a425b53d0ed28e103afd302e4b888
ericward-noaa <eric.ward@noaa.gov>
2024-12-11 17:57:26 -0800

    notes

--- data-raw/configuration_yellowtail.R
+++ data-raw/configuration_yellowtail.R
@@ -6,6 +6,12 @@
 # * for vessel_year, might want a different level scaling things might not have
 #   to give this to grid
 
+# Things to flag for Chantel:
+# 1. spatiotemporal + speed
+# 2. move dplyr::mutate(split_conception = ifelse(latitude > 40.1666667, "N", "S"))) to data formatting -- making
+# columns available for user
+# 3. could automate coefficient mapping
+# 4. edit lookup_grid() to add split_conception -- needs to be done for other splits
 library(dplyr)
 library(indexwc)
 configuration <- tibble::as_tibble(read.csv(
@@ -18,7 +24,7 @@ configuration <- configuration |>
 # Change the formula to add year : region interaction
 configuration$formula <- "catch_weight ~ 0 + fyear*split_conception + pass_scaled"
 configuration$knots <- 400
-configuration$spatiotemporal1 <- "off"
+# configuration$spatiotemporal1 <- "off" # can be turned off just for speed
 configuration$spatiotemporal2 <- "off" # based on EW's initial modeling, these worked best
 
 data <- configuration |>
@@ -49,35 +55,25 @@ dplyr::filter(data$data_filtered[[1]], catch_weight > 0) |>
 # Find variables that aren't identifiable for presence-absence model
 lm <- lm(formula = as.formula(configuration$formula),
          data = data$data_filtered[[1]])
-not_identifiable <- names(which(is.na(coef(lm))))
+#not_identifiable <- names(which(is.na(coef(lm))))
 # Find variables that aren't identifiable for positive model
 lm_pos <- lm(formula = as.formula(configuration$formula),
          data = dplyr::filter(data$data_filtered[[1]], catch_weight>0))
 pos_not_identifiable <- names(which(is.na(coef(lm_pos))))
 
 # Create variables to be not estimated/ mapped off
-.map <- names(coef(lm))
-.map[names(coef(lm)) %in% not_identifiable] <- NA
-.map <- factor(.map)
-.start <- rep(0, length(coef(lm)))
-.start[names(coef(lm)) %in% not_identifiable] <- -20
+# .map <- names(coef(lm))
+# .map[names(coef(lm)) %in% not_identifiable] <- NA
+# .map <- factor(.map)
+# .start <- rep(0, length(coef(lm)))
+# .start[names(coef(lm)) %in% not_identifiable] <- -20
 
-.map_pos <- names(coef(lm))
-.map_pos[names(coef(lm)) %in% pos_not_identifiable] <- NA
+coef_names <- names(coef(lm))
+.map_pos <- coef_names
+.map_pos[coef_names %in% pos_not_identifiable] <- NA
 .map_pos <- factor(.map_pos)
-.start_pos <- rep(0, length(coef(lm)))
-.start_pos[names(coef(lm)) %in% pos_not_identifiable] <- -20
-#
-# fit <- sdmTMB(catch_weight ~ 0 + fyear*split_conception + pass_scaled,
-#        offset = log(data$data_filtered[[1]]$effort),
-#        family = delta_gamma(),
-#        spatial="off",
-#        spatiotemporal = "off",
-#        data = data$data_filtered[[1]],
-#        control = sdmTMB::sdmTMBcontrol(
-#          map = list(b_j = .map_pos, b_j2 = .map_pos),
-#          start = list(b_j = .start_pos, b_j2 = .start_pos)
-#        ))
+.start_pos <- rep(0, length(coef_names))
+.start_pos[coef_names %in% pos_not_identifiable] <- -20
 
 best <- data |>
   dplyr::mutate(
