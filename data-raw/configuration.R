72ab5a9db1818d5c289d0c532bc8c2841a71ac63
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-08-06 08:26:40 -0700

    remove configuration github action

--- data-raw/configuration.R
+++ data-raw/configuration.R
@@ -1,13 +1,14 @@
-load(file.path("data", "configuration.rda"))
+# If adding/modifying configurations the following steps should be done:
+# 1. Open and edit the configuration.csv file in the data-raw folder
+# 2. Save and close the modified csv file
+# 3. Run the below script to update the configuration.rda file that is loaded with the package
+
+configuration <- read.csv(
+  file.path("data-raw", "configuration.csv"))
 
 usethis::use_data(
   configuration,
   overwrite = TRUE
 )
 
-utils::write.csv(
-  configuration,
-  file = here::here("data-raw", "configuration.csv"),
-  row.names = FALSE
-)
 
--- data-raw/create_configuration_csv.R
+++
@@ -1,7 +0,0 @@
-load(file.path("data", "configuration.rda"))
-
-utils::write.csv(
-  configuration,
-  file = here::here("data-raw", "configuration.csv"),
-  row.names = FALSE
-)
